848e6eea78d48cc68cf0b7f6cd41bcee7ab64a74
GuillermoReveco <guillermo.reveco@gmail.com>
2022-12-12 22:15:01 -0300

    endpoint despacho 1.0

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from models import db, User
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from routes import api
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -12,6 +13,9 @@ app.config["SQLALCHEMY_DATABASE_URI"]="mysql://root:@34.70.198.182/dgeslab"
 Migrate(app, db)
 db.init_app(app)
 CORS(app)
+
+app.register_blueprint(api)
+
 
 if __name__=='__main__':
     app.run(port=3100, debug=True)
